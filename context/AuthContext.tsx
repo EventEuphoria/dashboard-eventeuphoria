fcbdf6536d5484022abf216768c99577f566a3a0
nurrizkiamalia <rizkiamel9@gmail.com>
2024-07-13 17:38:12 +0700

    feat: add cookie sid non same site

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -4,7 +4,11 @@ import { createContext, useState, useContext, ReactNode, useEffect } from "react
 import { useRouter } from "next/navigation";
 import apiClient from "@/services/apiClient";
 import { User } from "@/types/datatypes";
-const TOKEN_KEY = 'jwtToken';
+import { parseCookies, setCookie, destroyCookie } from 'nookies';
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+const TOKEN_KEY = 'sid';
 
 interface AuthContextType {
     isAuthenticated: boolean;
@@ -14,17 +18,16 @@ interface AuthContextType {
     getToken: () => string | null;
 }
 
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
-
-export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     const [currentUser, setCurrentUser] = useState<User | null>(null);
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const router = useRouter();
-    
+
     useEffect(() => {
-        const token = getToken();
-        if(token){
+        const cookies = parseCookies();
+        const token = cookies[TOKEN_KEY];
+        if (token) {
             fetchProfile(token);
         } else {
             setIsLoading(false);
@@ -32,8 +35,8 @@ export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
         }
     }, []);
 
-    const fetchProfile = async(token: string) => {
-        try{
+    const fetchProfile = async (token: string) => {
+        try {
             const response = await apiClient.get('/profile', {
                 headers: {
                     Authorization: `Bearer ${token}`,
@@ -43,10 +46,10 @@ export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
             setIsAuthenticated(true);
             if (response.data.role !== 'ORGANIZER') {
                 router.push('/login');
-            } else{
+            } else {
                 router.push("/")
             }
-        } catch( error){
+        } catch (error) {
             setIsAuthenticated(false);
             removeToken();
             router.push('http://localhost:3000');
@@ -56,8 +59,8 @@ export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
     }
 
     const login = async (email: string, password: string) => {
-        try{
-            const response = await apiClient.post('/login', {email, password});
+        try {
+            const response = await apiClient.post('/login', { email, password });
             const { token } = response.data;
             setToken(token);
             await fetchProfile(token);
@@ -72,8 +75,9 @@ export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
     }
 
     const logout = async () => {
-        const token = getToken();
-        if(token){
+        const cookies = parseCookies();
+        const token = cookies[TOKEN_KEY];
+        if (token) {
             await apiClient.post('/logout', {}, {
                 headers: {
                     Authorization: `Bearer ${token}`
@@ -86,28 +90,22 @@ export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
         }
     }
 
-    const getToken = () => {
-        return localStorage.getItem(TOKEN_KEY);
-    }
+    const getToken = () => parseCookies()[TOKEN_KEY];
 
-    const setToken = (token: string) => {
-        localStorage.setItem(TOKEN_KEY, token);
-    }
+    const setToken = (token: string) => setCookie(null, TOKEN_KEY, token, { path: '/', domain: '.eventeuphoria.fun', secure: true, sameSite: 'none' });
 
-    const removeToken = () => {
-        localStorage.removeItem(TOKEN_KEY);
-    }
+    const removeToken = () => destroyCookie(null, TOKEN_KEY, { path: '/', domain: '.eventeuphoria.fun' });
 
-    return(
-        <AuthContext.Provider value={{ isAuthenticated, currentUser, login, logout, getToken}}>
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, currentUser, login, logout, getToken }}>
             {!isLoading && children}
         </AuthContext.Provider>
-    )
+    );
 }
 
 export const useAuth = () => {
     const context = useContext(AuthContext);
-    if(!context){
+    if (!context) {
         throw new Error('useAuth must be used within an AuthProvider');
     }
     return context;
